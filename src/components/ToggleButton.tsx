695885b7dba0c792c14fcaffe2907751aac723f2
Kater-auf-Dach <Kater-auf-Dach@users.noreply.github.com>
2023-01-24 19:22:10 +0300

    [Fix] Condition for icons render in ToggleButton

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { CloseIcon, DateIcon } from "./utils";
 
 interface ToggleButtonProps {
-    isEmpty?: boolean | undefined;
+    isEmpty: boolean;
 }
 
 const ToggleButton: React.FC<ToggleButtonProps> = (e: ToggleButtonProps): JSX.Element => {
-    return e.isEmpty ? <CloseIcon className="h-5 w-5" /> : <DateIcon className="h-5 w-5" />;
+    return e.isEmpty ? <DateIcon className="h-5 w-5" /> : <CloseIcon className="h-5 w-5" />;
 };
 
 export default ToggleButton;
