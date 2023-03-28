e8302f3017686f4615e23aa2f1dfce73986f5087
ruggero95 <ruggero.panzeri95@gmail.com>
2023-03-28 22:14:00 +0200

    remove z index

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -69,7 +69,7 @@ const Input: React.FC<Props> = (e: Props) => {
     const getLabelClasses = useCallback(() => {
         const labelColor =
             LABEL_COLOR["peerFocus"][primaryColor as keyof (typeof LABEL_COLOR)["peerFocus"]];
-        return `absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1 ${labelColor}`;
+        return `absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1 ${labelColor}`;
     }, [primaryColor]);
 
     const handleInputChange = useCallback(
