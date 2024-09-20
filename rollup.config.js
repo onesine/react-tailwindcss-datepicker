8bf60e15869b7b94e3e7f1eb5d430c3f1f7d69fe
sri <ritscher@sciendis.de>
2024-09-20 18:30:59 +0200

    build(package): update rollup, deps, and tsconfig

--- rollup.config.js
+++ rollup.config.js
@@ -2,27 +2,22 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
-const packageJson = require("./package.json");
-const options = require("./tsconfig.json");
-
 module.exports = {
     input: "src/index.tsx",
     output: [
         {
-            file: packageJson.main,
+            file: "dist/index.cjs.js",
             format: "cjs",
-            exports: "auto",
             sourcemap: true,
             inlineDynamicImports: true
         },
         {
-            file: packageJson.module,
+            file: "dist/index.esm.js",
             format: "esm",
-            exports: "auto",
             sourcemap: true,
             inlineDynamicImports: true
         }
     ],
     external: ["react", "dayjs"],
-    plugins: [resolve(), commonjs(), typescript({ ...options.compilerOptions, jsx: "react" })]
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })]
 };
